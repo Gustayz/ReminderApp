7777f1f430a04b7e1ce01bfcc7df6760c561b9f7
Gustayz <114109045+Gustayz@users.noreply.github.com>
2023-12-07 18:34:15 -0500

    Update: 12/7/2023
    Added a method called Menu(), and will be use as the menu of the program, that will take the user input and use it to determine the next action.

--- ReminderApp/Program.cs
+++ ReminderApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, LORDD World!");
+﻿using System;
+
+namespace ReminderApp
+    {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}, how can i help you today?");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Choose an option below");
+            Console.WriteLine("----------------------*");
+            Console.WriteLine("*1. Add a reminder    *");
+            Console.WriteLine("*2. Remove a reminder *");
+            Console.WriteLine("*3. Edit a reminder   *");
+            Console.WriteLine("*4. Exit program      *");
+            Console.WriteLine("----------------------*");
+        }
+    }
+
+    }
